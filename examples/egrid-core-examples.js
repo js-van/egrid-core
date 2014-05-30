884c9863b444b09ad6b883194ba329b15a76c5e2
Yosuke ONOUE <onoue@likr-lab.com>
2014-05-30 22:01:21 +0900

    Add scatter plot matrix of centrality

--- examples/egrid-core-examples.js
+++ examples/egrid-core-examples.js
@@ -1,3 +1,135 @@
+function scatterMatrix(data) {
+  var width = 960,
+      size = 150,
+      padding = 19.5;
+  var x = d3.scale.linear()
+      .range([padding / 2, size - padding / 2]);
+  var y = d3.scale.linear()
+      .range([size - padding / 2, padding / 2]);
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+      .ticks(5);
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(5);
+  var color = d3.scale.category10();
+
+  return function(selection) {
+    var domainByTrait = {},
+        traits = d3.keys(data[0]).filter(function(d) { return d !== "species"; }),
+        n = traits.length;
+
+    traits.forEach(function(trait) {
+      domainByTrait[trait] = d3.extent(data, function(d) { return d[trait]; });
+    });
+
+    xAxis.tickSize(size * n);
+    yAxis.tickSize(-size * n);
+
+    var brush = d3.svg.brush()
+        .x(x)
+        .y(y)
+        .on("brushstart", brushstart)
+        .on("brush", brushmove)
+        .on("brushend", brushend);
+
+    var svg = selection
+        .attr("width", size * n + padding)
+        .attr("height", size * n + padding)
+      .append("g")
+        .attr("transform", "translate(" + padding + "," + padding / 2 + ")");
+  
+    svg.selectAll(".x.axis")
+        .data(traits)
+      .enter().append("g")
+        .attr("class", "x axis")
+        .attr("transform", function(d, i) { return "translate(" + (n - i - 1) * size + ",0)"; })
+        .each(function(d) { x.domain(domainByTrait[d]); d3.select(this).call(xAxis); });
+  
+    svg.selectAll(".y.axis")
+        .data(traits)
+      .enter().append("g")
+        .attr("class", "y axis")
+        .attr("transform", function(d, i) { return "translate(0," + i * size + ")"; })
+        .each(function(d) { y.domain(domainByTrait[d]); d3.select(this).call(yAxis); });
+  
+    var cell = svg.selectAll(".cell")
+        .data(cross(traits, traits))
+      .enter().append("g")
+        .attr("class", "cell")
+        .attr("transform", function(d) { return "translate(" + (n - d.i - 1) * size + "," + d.j * size + ")"; })
+        .each(plot);
+  
+    // Titles for the diagonal.
+    cell.filter(function(d) { return d.i === d.j; }).append("text")
+        .attr("x", padding)
+        .attr("y", padding)
+        .attr("dy", ".71em")
+        .text(function(d) { return d.x; });
+  
+    cell.call(brush);
+  
+    function plot(p) {
+      var cell = d3.select(this);
+  
+      x.domain(domainByTrait[p.x]);
+      y.domain(domainByTrait[p.y]);
+  
+      cell.append("rect")
+          .attr("class", "frame")
+          .attr("x", padding / 2)
+          .attr("y", padding / 2)
+          .attr("width", size - padding)
+          .attr("height", size - padding);
+  
+      cell.selectAll("circle")
+          .data(data)
+        .enter().append("circle")
+          .attr("cx", function(d) { return x(d[p.x]); })
+          .attr("cy", function(d) { return y(d[p.y]); })
+          .attr("r", 3)
+          .style("fill", function(d) { return color(d.species); });
+    }
+  
+    var brushCell;
+  
+    // Clear the previously-active brush, if any.
+    function brushstart(p) {
+      if (brushCell !== this) {
+        d3.select(brushCell).call(brush.clear());
+        x.domain(domainByTrait[p.x]);
+        y.domain(domainByTrait[p.y]);
+        brushCell = this;
+      }
+    }
+  
+    // Highlight the selected circles.
+    function brushmove(p) {
+      var e = brush.extent();
+      svg.selectAll("circle").classed("inactive", function(d) {
+        return e[0][0] > d[p.x] || d[p.x] > e[1][0]
+            || e[0][1] > d[p.y] || d[p.y] > e[1][1];
+      });
+    }
+  
+    // If the brush is empty, select all circles.
+    function brushend() {
+      if (brush.empty()) svg.selectAll(".inactive").classed("inactive", false);
+    }
+  
+    function cross(a, b) {
+      var c = [], n = a.length, m = b.length, i, j;
+      for (i = -1; ++i < n;) for (j = -1; ++j < m;) c.push({x: a[i], i: i, y: b[j], j: j});
+      return c;
+    }
+  
+    d3.select(self.frameElement).style("height", size * n + padding + 20 + "px");
+  }
+}
+
+
 var app = angular.module('egrid-core-example', ['ui.router'])
   .config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/color');
@@ -117,9 +249,22 @@ var app = angular.module('egrid-core-example', ['ui.router'])
         controller: function($scope, data) {
           var graph = egrid.core.graph.graph();
           var grid = graph(data.data.nodes, data.data.links);
-          var degreeCentrality = egrid.core.network.centrality.degree;
-          var closenessCentrality = egrid.core.network.centrality.closeness(function() {return 1});
-          var betweennessCentrality = egrid.core.network.centrality.betweenness();
+          var weight = grid.vertices().map(function(u) {return grid.get(u).weight});
+          var degreeCentrality = egrid.core.network.centrality.degree(grid);
+          var closenessCentrality = egrid.core.network.centrality.closeness(function() {return 1})(grid);
+          var betweennessCentrality = egrid.core.network.centrality.betweenness()(grid);
+
+          var weights = grid.vertices().map(function(u) {
+            return {
+              weight: weight[u],
+              degreeCentrality: degreeCentrality[u],
+              closenessCentrality: closenessCentrality[u],
+              betweennessCentrality: betweennessCentrality[u]
+            };
+          });
+
+          d3.select('svg.scatter-plot')
+            .call(scatterMatrix(weights));
 
           var egm = egrid.core.egm()
             .size([600, 600]);
@@ -131,12 +276,14 @@ var app = angular.module('egrid-core-example', ['ui.router'])
           $scope.centrality = 'degree';
           $scope.$watch('centrality', function(oldValue, newValue) {
             var centrality;
-            if ($scope.centrality == 'degree') {
-              centrality = degreeCentrality(grid);
+            if ($scope.centrality == 'weight') {
+              centrality = weight;
+            } else if ($scope.centrality == 'degree') {
+              centrality = degreeCentrality;
             } else if ($scope.centrality == 'closeness') {
-              centrality = closenessCentrality(grid);
+              centrality = closenessCentrality;
             } else {
-              centrality = betweennessCentrality(grid);
+              centrality = betweennessCentrality;
             }
 
             var extent = d3.extent(grid.vertices(), function(u) {
