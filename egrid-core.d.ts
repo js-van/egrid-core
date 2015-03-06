83f6b5f84a6b830afec02d67c614535e6b83adf6
Yosuke ONOUE <onoue@likr-lab.com>
2015-03-06 17:19:15 +0900

    Fix generics to divide vertex-property and edge-property in .d.ts

--- egrid-core.d.ts
+++ egrid-core.d.ts
@@ -12,11 +12,7 @@ declare module egrid {
       links: LinkJson[];
     }
 
-    interface BaseEdgeProps {
-      coefficient: number;
-    }
-
-    interface Graph<T> {
+    interface Graph<P, Q> {
       vertices(): number[];
       edges(): [number, number][];
       adjacentVertices(u: number): number[];
@@ -29,33 +25,40 @@ declare module egrid {
       numEdges(): number;
       vertex(u: number): number;
       edge(u: number, v: number): boolean;
-      addEdge(u: number, v: number, prop?: T): [number, number];
+      addEdge(u: number, v: number, prop?: Q): [number, number];
       removeEdge(u: number, v: number): void;
-      addVertex(prop: T, u?: number): number;
+      addVertex(prop: P, u?: number): number;
       clearVertex(u: number): void;
       removeVertex(u: number): void;
-      get(u: number): T;
-      get(u: number, v: number): BaseEdgeProps;
-      set(u: number, prop: T): void;
-      set(u: number, v: number, prop: T): void;
+      get(u: number): P;
+      get(u: number, v: number): Q;
+      set(u: number, prop: P): void;
+      set(u: number, v: number, prop: Q): void;
     }
 
     module graph {
-      function adjacencyList<T>(): Graph<T>;
+      function adjacencyList<P, Q>(): Graph<P, Q>;
 
-      function dumpJSON<T>(graph: Graph<T>): GraphJson;
+      function dumpJSON(graph: Graph<any, any>): GraphJson;
 
-      interface WarshallFloyd<T> {
-        (graph: Graph<T>): {[u: number]: {[v: number]: number}};
-        weight(): (node: any) => number;
-        weight(f: (node: any) => number): WarshallFloyd<T>;
+      interface WarshallFloyd<P> {
+        (graph: Graph<P, any>): {[u: number]: {[v: number]: number}};
+        weight(): (node: P) => number;
+        weight(f: (node: P) => number): WarshallFloyd<P>;
       }
 
-      function warshallFloyd<T>(): WarshallFloyd<T>;
+      function warshallFloyd<P>(): WarshallFloyd<P>;
     }
 
-    interface Grid<T> {
-      graph(): Graph<T>;
+    interface GridNode {
+      text: string;
+    }
+
+    interface GridLink {
+    }
+
+    interface Grid<P extends GridNode, Q extends GridLink> {
+      graph(): Graph<P, Q>;
       addConstruct(text: string): number;
       removeConstruct(u: number): void;
       updateConstruct(u: number, property: string, value: any): void;
@@ -63,8 +66,8 @@ declare module egrid {
       removeEdge(u: number, v: number): void;
       ladderUp(u: number, text: string): number;
       ladderDown(u: number, text: string): number;
-      merge(u: number, v: number, f?: (u: number, v: number) => any): number;
-      group(us: number[], attrs?: any): number;
+      merge(u: number, v: number, f?: (u: number, v: number) => P): number;
+      group(us: number[], attrs?: P[]): number;
       ungroup(u: number): void;
       canUndo(): boolean;
       canRedo(): boolean;
@@ -72,7 +75,7 @@ declare module egrid {
       redo(): void;
     }
 
-    function grid<T>(vertices?: any[], edges?: any[]): Grid<T>;
+    function grid<P extends GridNode, Q extends GridLink>(vertices?: any[], edges?: any[]): Grid<P, Q>;
 
     interface VertexButton {
       icon: string;
@@ -83,122 +86,123 @@ declare module egrid {
       scale?: number;
     }
 
-    interface EGM<T> {
+    interface D3Callable {
+      (selection: D3.Selection): void;
+      (selection: D3.EnterSelection): void;
+      (selection: D3.Transition.Transition): void;
+    }
+
+    interface EGM<P, Q> extends D3Callable {
       /**
        * global attributes
        */
       backgroundColor(): string;
-      backgroundColor(arg: string): EGM<T>;
+      backgroundColor(arg: string): EGM<P, Q>;
       contentsMargin(): number;
-      contentsMargin(arg: number): EGM<T>;
+      contentsMargin(arg: number): EGM<P, Q>;
       contentsScaleMax(): number;
-      contentsScaleMax(arg: number): EGM<T>;
+      contentsScaleMax(arg: number): EGM<P, Q>;
       dagreEdgeSep(): number;
-      dagreEdgeSep(arg: number): EGM<T>;
+      dagreEdgeSep(arg: number): EGM<P, Q>;
       dagreNodeSep(): number;
-      dagreNodeSep(arg: number): EGM<T>;
-      dagreRanker(): (g: Graph<T>) => void;
-      dagreRanker(arg: (g: Graph<T>) => void): EGM<T>;
+      dagreNodeSep(arg: number): EGM<P, Q>;
+      dagreRanker(): (g: Graph<any, any>) => void;
+      dagreRanker(arg: (g: Graph<any, any>) => void): EGM<P, Q>;
       dagreRankDir(): string;
-      dagreRankDir(arg: string): EGM<T>;
+      dagreRankDir(arg: string): EGM<P, Q>;
       dagreRankSep(): number;
-      dagreRankSep(arg: number): EGM<T>;
+      dagreRankSep(arg: number): EGM<P, Q>;
       edgeInterpolate(): string;
-      edgeInterpolate(arg: string): EGM<T>;
+      edgeInterpolate(arg: string): EGM<P, Q>;
       edgeTension(): number;
-      edgeTension(arg: number): EGM<T>;
+      edgeTension(arg: number): EGM<P, Q>;
       enableClickVertex(): boolean;
-      enableClickVertex(arg: boolean): EGM<T>;
+      enableClickVertex(arg: boolean): EGM<P, Q>;
       enableZoom(): boolean;
-      enableZoom(arg: boolean): EGM<T>;
+      enableZoom(arg: boolean): EGM<P, Q>;
       lowerStrokeColor(): string;
-      lowerStrokeColor(arg: string): EGM<T>;
+      lowerStrokeColor(arg: string): EGM<P, Q>;
       maxTextLength(): number;
-      maxTextLength(arg: number): EGM<T>;
-      onClickVertex(): (d: T, u: number) => any;
-      onClickVertex(arg: (d: T, u: number) => any): EGM<T>;
+      maxTextLength(arg: number): EGM<P, Q>;
+      onClickVertex(): (d: P, u: number) => any;
+      onClickVertex(arg: (d: P, u: number) => any): EGM<P, Q>;
       selectedStrokeColor(): string;
-      selectedStrokeColor(arg: string): EGM<T>;
+      selectedStrokeColor(arg: string): EGM<P, Q>;
       strokeColor(): string;
-      strokeColor(arg: string): EGM<T>;
+      strokeColor(arg: string): EGM<P, Q>;
       textSeparator(): (text: string) => string[];
-      textSeparator(arg: (text: string) => string[]): EGM<T>;
+      textSeparator(arg: (text: string) => string[]): EGM<P, Q>;
       vertexButtons(): VertexButton[];
-      vertexButtons(arg: VertexButton[]): EGM<T>;
+      vertexButtons(arg: VertexButton[]): EGM<P, Q>;
       size(): number[];
-      size(arg: number[]): EGM<T>;
+      size(arg: number[]): EGM<P, Q>;
       upperStrokeColor(): string;
-      upperStrokeColor(arg: string): EGM<T>;
+      upperStrokeColor(arg: string): EGM<P, Q>;
 
       /**
        * vertex attributes
        */
-      vertexAveilability(): (node: T, u: number) => boolean;
-      vertexAveilability(arg: (node: T, u: number) => boolean): EGM<T>;
-      vertexColor(): (node: T, u: number) => string;
-      vertexColor(arg: (node: T, u: number) => string): EGM<T>;
-      vertexFontWeight(): (node: T, u: number) => string;
-      vertexFontWeight(arg: (node: T, u: number) => string): EGM<T>;
-      vertexOpacity(): (node: T, u: number) => number;
-      vertexOpacity(arg: (node: T, u: number) => number): EGM<T>;
-      vertexScale(): (node: T, u: number) => number;
-      vertexScale(arg: (node: T, u: number) => number): EGM<T>;
-      vertexStrokeWidth(): (node: T, u: number) => number;
-      vertexStrokeWidth(arg: (node: T, u: number) => number): EGM<T>;
-      vertexText(): (node: T, u: number) => string;
-      vertexText(arg: (node: T, u: number) => string): EGM<T>;
-      vertexVisibility(): (node: T, u: number) => boolean;
-      vertexVisibility(arg: (node: T, u: number) => boolean): EGM<T>;
+      vertexAveilability(): (node: P, u: number) => boolean;
+      vertexAveilability(arg: (node: P, u: number) => boolean): EGM<P, Q>;
+      vertexColor(): (node: P, u: number) => string;
+      vertexColor(arg: (node: P, u: number) => string): EGM<P, Q>;
+      vertexFontWeight(): (node: P, u: number) => string;
+      vertexFontWeight(arg: (node: P, u: number) => string): EGM<P, Q>;
+      vertexOpacity(): (node: P, u: number) => number;
+      vertexOpacity(arg: (node: P, u: number) => number): EGM<P, Q>;
+      vertexScale(): (node: P, u: number) => number;
+      vertexScale(arg: (node: P, u: number) => number): EGM<P, Q>;
+      vertexStrokeWidth(): (node: P, u: number) => number;
+      vertexStrokeWidth(arg: (node: P, u: number) => number): EGM<P, Q>;
+      vertexText(): (node: P, u: number) => string;
+      vertexText(arg: (node: P, u: number) => string): EGM<P, Q>;
+      vertexVisibility(): (node: P, u: number) => boolean;
+      vertexVisibility(arg: (node: P, u: number) => boolean): EGM<P, Q>;
 
       /**
        * edge attributes
        */
       edgeColor(): (u: number, v: number) => string;
-      edgeColor(arg: (u: number, v: number) => string): EGM<T>;
+      edgeColor(arg: (u: number, v: number) => string): EGM<P, Q>;
       edgeOpacity(): (u: number, v: number) => number;
-      edgeOpacity(arg: (u: number, v: number) => number): EGM<T>;
+      edgeOpacity(arg: (u: number, v: number) => number): EGM<P, Q>;
       edgeText(): (u: number, v: number) => string;
-      edgeText(arg: (u: number, v: number) => string): EGM<T>;
+      edgeText(arg: (u: number, v: number) => string): EGM<P, Q>;
       edgeVisibility(): (u: number, v: number) => boolean;
-      edgeVisibility(arg: (u: number, v: number) => boolean): EGM<T>;
+      edgeVisibility(arg: (u: number, v: number) => boolean): EGM<P, Q>;
       edgeWidth(): (u: number, v: number) => number;
-      edgeWidth(arg: (u: number, v: number) => number): EGM<T>;
-
-      /**
-       * draw the graph
-       */
-      (selection: D3.Selection): void;
+      edgeWidth(arg: (u: number, v: number) => number): EGM<P, Q>;
 
       /**
        * centering the svg
        */
-      center(arg?: EGMCenterOptions): (selection: D3.Selection) => void;
+      center(arg?: EGMCenterOptions): D3Callable;
 
       /**
        * apply styles to the svg
        */
-      css(): (selection: D3.Selection) => void;
+      css(): D3Callable;
 
       /**
        * resize the svg
        */
-      resize(width: number, height: number): (selection: D3.Selection) => void;
+      resize(width: number, height: number): D3Callable;
 
       /**
        * update color attributes
        */
-      updateColor(): (selection: D3.Selection) => void;
+      updateColor(): D3Callable;
     }
 
-    function egm<T>(): EGM<T>;
+    function egm<P, Q>(): EGM<P, Q>;
 
     module network {
       module centrality {
-        function katz<T>(graph: Graph<T>): {[u: number]: number};
+        function katz<P, Q>(graph: Graph<P, Q>): {[u: number]: number};
       }
 
       module community {
-        function newman<T>(graph: Graph<T>): number[][];
+        function newman<P, Q>(graph: Graph<P, Q>): number[][];
       }
     }
   }
